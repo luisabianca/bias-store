89b59dce326b1916c0cd7e7c01ed151adbd6533d
luisabianca <luisabianca93@gmail.com>
2023-10-04 00:09:58 -0300

    init products api

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 "use client";
 
 import Header from "@/components/Header/Header";
+import Products from "@/components/Products/Products";
 import "./page.css"
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Products />
     </>
   )
 }
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Products.css"
+
+const Products = () => {
+  return (
+    <section className='products container'>Products</section>
+  )
+}
+
+export default Products;
