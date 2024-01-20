4890784c46818f1dc9bb161dd91c8ed4cc2eef67
Bianca Santos <bianca.luisa@corebiz.ag>
2024-01-19 22:53:40 -0300

    create a card products component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,26 @@
+import { BsFillCartPlusFill } from "react-icons/bs";
+import "./ProductCard.css"
+import formatCurrency from "@/utils/formatCurrency";
+
+
+const ProductCard = ({data}) => {
+
+  const {title, thumbnail, price} = data;
+  return (
+    <section className="product-card">
+
+      <img src={thumbnail.replace(/\w\.jpg/gi, "W.jpg")} alt="product" className="card__image" />
+      <div className="card__infos">
+        <h2 className="card__price">{formatCurrency(price, 'BLR')}</h2>
+        <h2 className="card__title">{title}</h2>
+      </div>
+
+      <button type="button" className="button__add-cart">
+        <BsFillCartPlusFill />
+      </button>
+
+    </section>
+  )
+}
+
+export default ProductCard;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Products.css"
+import fetchProducts from '@/api/fetchProducts';
+import ProductCard from '../ProductCard/ProductCard';
 
 const Products = () => {
+  const [ products, setProducts ] = useState([]);
+
+  useEffect(() => {
+
+    fetchProducts('iphone').then((response) => {
+      setProducts(response);
+    })
+
+  }, [])
+
   return (
-    <section className='products container'>Products</section>
+    <section className='products container'>
+     {
+      products.map((product) => <ProductCard key={product.id} data={product} />)
+     }
+    </section>
   )
 }
 
